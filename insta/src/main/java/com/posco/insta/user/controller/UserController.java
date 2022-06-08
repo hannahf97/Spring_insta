af7645611b8bb762093b81bd61fec167d709b37b
hannahf97 <46432606+hannahf97@users.noreply.github.com>
2022-06-08 14:23:23 +0900

    [fix] Mapping to RESTful

--- insta/src/main/java/com/posco/insta/user/controller/UserController.java
+++ insta/src/main/java/com/posco/insta/user/controller/UserController.java
@@ -24,7 +24,7 @@ public class UserController {
        return userService.findUserById(userDto);
     }
 
-    @PostMapping("/create")
+    @PostMapping("/")
     public int createPost(@RequestParam String userId, @RequestParam String img, @RequestParam String name, @RequestParam String password){
         UserDto userDto = new UserDto();
         userDto.setUserId(userId);
@@ -34,5 +34,6 @@ public class UserController {
 
         return userService.insertUser(userDto);
     }
+
 
 }
