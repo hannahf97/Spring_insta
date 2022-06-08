e5a484327175441d34842cd4feb459ba101a8bc5
hannahf97 <46432606+hannahf97@users.noreply.github.com>
2022-06-08 12:34:08 +0900

    [Add] Controller

--- insta/src/main/java/com/posco/insta/TestController.java
+++ insta/src/main/java/com/posco/insta/TestController.java
@@ -5,32 +5,8 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class TestController {
     @GetMapping("/")
-    public String test(){
+    public String test() {
         return "test123";
     }
-
-    @GetMapping("/{id}")
-    public String testId(@PathVariable String id){
-        return id;
-    }
-
-    @PostMapping("/")
-    public String testPost(){
-        return "test";
-    }
-
-    @DeleteMapping("/")
-    public String testDelete(){
-        return "hello world";
-    }
-
-    @DeleteMapping("/{id}")
-    public String testDelete(@PathVariable String id){
-        return id;
-    }
-
-    @PutMapping("/")
-    public String testPut(){
-        return "hello world";
-    }
 }
+
--- insta/src/main/java/com/posco/insta/user/controller/UserController.java
+++ insta/src/main/java/com/posco/insta/user/controller/UserController.java
@@ -1,7 +1,30 @@
 package com.posco.insta.user.controller;
 
+import com.posco.insta.user.model.UserDto;
+import com.posco.insta.user.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/user")
+import java.util.List;
+
+@RestController
+@RequestMapping("user")
 public class UserController {
+    @Autowired
+    UserServiceImpl userService;
+    @GetMapping("/")
+    public List<UserDto> getUser(){
+        return userService.findUser();
+    }
+
+    @GetMapping("/{id}")
+    public UserDto getUserById(@PathVariable String id){
+        UserDto userDto = new UserDto();
+        userDto.setId(Integer.valueOf(id));
+       return userService.findUserById(userDto);
+    }
+
 }
