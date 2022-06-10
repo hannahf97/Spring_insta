a4139cf6a0276a283a42dc8b47c7000c87f7deb0
hannahf97 <46432606+hannahf97@users.noreply.github.com>
2022-06-10 09:55:05 +0900

    [fix] swagger ui with api2 and groupName

--- insta/src/main/java/com/posco/insta/config/SwaggerConfig.java
+++ insta/src/main/java/com/posco/insta/config/SwaggerConfig.java
@@ -6,17 +6,33 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 @Configuration
+@EnableSwagger2
 public class SwaggerConfig {
     @Bean
     public Docket api(){
         return new Docket(DocumentationType.OAS_30)
+                .groupName("user")
                 .select()
-                //.apis(RequestHandlerSelectors.basePackage("com.posco.insta.user.controller"))
-                .apis(RequestHandlerSelectors.basePackage("com.posco.insta"))
+                .apis(RequestHandlerSelectors.basePackage("com.posco.insta.user.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    @Bean
+    public Docket api2(){
+        return new Docket(DocumentationType.OAS_30)
+                .groupName("post")
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.posco.insta.post.controller"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
